38448f57994283b2e334f819e603614a8723fc6c
github nitf <2tf2003@gmail.com>
2019-05-21 08:41:31 +0900

    [add] getGames

--- src/NitEnergy/game/GameHandler.php
+++ src/NitEnergy/game/GameHandler.php
@@ -8,16 +8,36 @@ class GameHandler
     /** @var array */
     private static $games = [];
 
+    /**
+     * @param Game $game
+     * Create new Game instance.
+     */
     public static function open(Game $game): void
     {
         self::$games[$game->getName()] = $game;
     }
 
+    /**
+     * @param Game $game
+     * Close Game.
+     */
     public static function close(Game $game): void
     {
         unset(self::$games[$game->getName()]);
     }
 
+    /**
+     * @return array
+     */
+    public static function getGames(): array
+    {
+        return self::$games;
+    }
+
+    /**
+     * @param string $name
+     * @return Game
+     */
     public static function getGame(string $name): Game
     {
         return self::$games[$name];
