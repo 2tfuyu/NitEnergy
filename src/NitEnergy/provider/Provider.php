81eac851640f9230bfed4c6f81ec4b863e2bd6ad
github nitf <2tf2003@gmail.com>
2019-05-20 12:26:51 +0900

    [fix] camelCase

--- src/NitEnergy/provider/Provider.php
+++ src/NitEnergy/provider/Provider.php
@@ -8,12 +8,26 @@ use pocketmine\utils\Config;
 abstract class Provider
 {
 
+    /** @var Config  */
     private $config;
+
+    /** @var array  */
     private $data;
 
-    public function __construct(string $name, array $input_data)
+    public function __construct(string $name, array $inputData)
     {
-        $this->config = new Config(Main::getInstance()->getDataFolder() . $name, Config::YAML, $input_data);
-        $data = $this->
+        $this->config = new Config(Main::getPath() . $name, Config::YAML, $inputData);
+        $this->data = $this->config->getAll(true);
+    }
+
+    public function set(string $key, array $inputData): void
+    {
+        $this->data[$key] = $inputData;
+    }
+
+    public function save(): void
+    {
+        $this->config->setAll($this->data);
+        $this->config->save();
     }
 }
