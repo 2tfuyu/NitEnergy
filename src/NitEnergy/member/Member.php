04ed5a74d5edf7c8996f2ffe1579834425d2d761
github nitf <2tf2003@gmail.com>
2019-05-21 08:41:03 +0900

    [add] addKill, Death

--- src/NitEnergy/member/Member.php
+++ src/NitEnergy/member/Member.php
@@ -13,6 +13,12 @@ class Member
     /** @var string */
     private $team;
 
+    /** @var int */
+    private $kill;
+
+    /** @var int */
+    private $death;
+
     public function __construct(Player $player)
     {
         $this->player = $player;
@@ -23,16 +29,51 @@ class Member
         $this->team = $name;
     }
 
+    /**
+     * @return string
+     */
     public function getTeam(): string
     {
         return $this->team;
     }
 
+    public function addKill(): void
+    {
+        $this->kill++;
+    }
+
+    public function addDeath(): void
+    {
+        $this->death++;
+    }
+
+    /**
+     * @return int
+     */
+    public function getKill(): int
+    {
+        return $this->kill;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDeath(): int
+    {
+        return $this->death;
+    }
+
+    /**
+     * @return Player
+     */
     public function getPlayer(): Player
     {
         return $this->player;
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->player->getName();
