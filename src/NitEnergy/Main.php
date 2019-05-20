03ef40b82ad616deaac58dc81b1e0c162619d1d8
github nitf <2tf2003@gmail.com>
2019-05-20 12:29:37 +0900

    [fix] Remove getInstance

--- src/NitEnergy/Main.php
+++ src/NitEnergy/Main.php
@@ -4,19 +4,34 @@ namespace NitEnergy;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\TaskScheduler;
 
 class Main extends PluginBase implements Listener
 {
 
-    private static $instance;
+    /** @var $path */
+    private static $path;
+
+    private static $scheduler;
 
     public function onEnable(): void
     {
-        self::$instance = $this;
+        self::$path = $this->getDataFolder();
+        self::$scheduler = $this->getScheduler();
+
+        if (!file_exists(self::$path))
+        {
+            @mkdir(self::$path);
+        }
     }
 
-    public static function getInstance(): self
+    public static function getPath(): string
     {
-        return self::$instance;
+        return self::$path;
+    }
+
+    public static function _getScheduler(): TaskScheduler
+    {
+        return self::$scheduler;
     }
 }
