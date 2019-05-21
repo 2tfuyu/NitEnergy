bbdb31bdd2e62ebf461e2a42d73ea9101048e1c0
github nitf <2tf2003@gmail.com>
2019-05-21 18:08:33 +0900

    [add] JoinEvent class

---
+++ src/NitEnergy/event/JoinEvent.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace NitEnergy\event;
+
+use NitEnergy\Main;
+use NitEnergy\provider\PlayerData;
+use NitEnergy\provider\Provider;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class JoinEvent extends Provider implements Listener, PlayerData
+{
+
+    /** @var string  */
+    const FILE_PATH = "%DATA_PATH%/Player";
+
+    /** @var string  */
+    const FILE_NAME = "Player.yml";
+
+    public function __construct(Main $plugin)
+    {
+        parent::__construct(self::FILE_PATH, self::FILE_NAME, null);
+        Server::getInstance()->getPluginManager()->registerEvents($this, $plugin);
+    }
+
+    public function onJoin(PlayerJoinEvent $e): void
+    {
+        $player = $e->getPlayer();
+
+        if (!$this->existsAccount($player))
+        {
+            $this->createAccount($player);
+        }
+    }
+
+    /**
+     * @param Player $player
+     * @return bool
+     * Check the account existence.
+     */
+    public function existsAccount(Player $player): bool
+    {
+        return $this->exists($player->getName());
+    }
+
+    /**
+     * @param Player $player
+     */
+    public function createAccount(Player $player): void
+    {
+        $data = [
+            "name" => $player->getName(),
+            "record" => [],
+        ];
+        $this->set($player->getName(), $data);
+    }
+
+    /**
+     * @param Player $player
+     */
+    public function removeAccount(Player $player): void
+    {
+        // TODO: Implement removeAccount() method.
+    }
+}
