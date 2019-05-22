e2ae0aac37d45d18b067fc2d22d0c5f070c9e856
github nitf <2tf2003@gmail.com>
2019-05-22 11:20:19 +0900

    [add] set

--- src/NitEnergy/command/SystemDB.php
+++ src/NitEnergy/command/SystemDB.php
@@ -11,8 +11,21 @@ class SystemDB extends Provider
         parent::__construct($path, $name, $inputData);
     }
 
+    /**
+     * @param string $key
+     * @return array
+     */
     public function get(string $key): array
     {
         return parent::get($key);
+    }
+
+    /**
+     * @param string $key
+     * @param array $input_data
+     */
+    public function set(string $key, array $input_data): void
+    {
+        parent::set($key, $input_data);
     }
 }
