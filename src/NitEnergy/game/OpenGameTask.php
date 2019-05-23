fde3e59f3db8030b1cb02d47e233b348232b6018
github nitf <2tf2003@gmail.com>
2019-05-23 12:56:37 +0900

    [add] OpenGameTask

---
+++ src/NitEnergy/game/OpenGameTask.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NitEnergy\game;
+
+use pocketmine\scheduler\Task;
+
+class OpenGameTask extends Task
+{
+
+    public function onRun(int $currentTick)
+    {
+        $gameList = GameHandler::getGameList();
+        foreach ($gameList as $gameName)
+        {
+            $game = GameHandler::getGame($gameName);
+            if (!$game === null)
+            {
+                GameHandler::open(GameHandler::createGame($game));
+            }
+        }
+    }
+}
