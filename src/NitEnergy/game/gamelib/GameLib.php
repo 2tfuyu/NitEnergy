2c185da0e8ad746cfb40fe608569b40eabb44673
github nitf <2tf2003@gmail.com>
2019-06-14 14:29:41 +0900

    [add] CTW functions

--- src/NitEnergy/game/gamelib/GameLib.php
+++ src/NitEnergy/game/gamelib/GameLib.php
@@ -3,6 +3,11 @@
 namespace NitEnergy\game\gamelib;
 
 use NitEnergy\member\Member;
+use pocketmine\block\Block;
+use pocketmine\block\BlockIds;
+use pocketmine\block\utils\ColorBlockMetaHelper;
+use pocketmine\block\Wool;
+use pocketmine\math\Vector3;
 
 class GameLib
 {
@@ -68,6 +73,10 @@ class GameLib
         }
     }
 
+    /**
+     * @param callable $function
+     * @param array $teams
+     */
     public static function processTeams(callable $function, array $teams): void
     {
         foreach ($teams as $team) {
@@ -100,6 +109,43 @@ class GameLib
             $i++;
         }
         return $teams;
+    }
+
+    /**
+     * @param array $pos
+     * @param Block $wool
+     * @return bool
+     */
+    public static function isWoolPlace(array $pos, Block $wool): bool
+    {
+        return (
+            $wool->getX() === $pos["X"] &&
+            $wool->getY() === $pos["Y"] &&
+            $wool->getZ() === $pos["Z"]
+        );
+    }
+
+    public static function isAllWoolPlaced(array $wools): bool
+    {
+        foreach ($wools as $wool) {
+            if (!$wool) return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param array $wools
+     * @param int $meta
+     * @return int|null
+     */
+    public static function isTeamWool(array $wools, int $meta): ?int
+    {
+        foreach ($wools as $wool) {
+            if (key($wool) === $meta) {
+                $id = $wool;
+            }
+        }
+        return $wool ?? null;
     }
 
     /**
